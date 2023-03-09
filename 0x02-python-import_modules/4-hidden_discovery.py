7053522f7e5e81bcf3af5d55a3226ae9ebaba12c
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-03-09 14:57:14 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-if _name_ == "_main_":
-    """ print all names defined by hidden_4 module."""
-    import hidden_4
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "_" :
-            print (name)
+import hidden_4
+
+
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    discovr()
